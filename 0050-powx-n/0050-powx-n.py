daea62a96add339bdebd256f9e78feda5a15f5ed
tanaypatil <tanaypatil197@gmail.com>
2022-10-23 00:36:11 +0530

    Time: 67 ms (14.46%), Space: 13.9 MB (68.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,15 @@
+def power(x, n):
+    if not x:
+        return 0
+    if not n:
+        return 1
+    n = n
+    if n%2:
+        return x*power(x**2, (n-1)//2)
+    return power(x**2, n//2)
+
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        return power(x, n) if n >= 0 else power(1/x, -1*n)
         
