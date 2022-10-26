aed4e8fd3ff3761030a6ff7aa606a1ff667ba8f3
tanaypatil <tanaypatil197@gmail.com>
2022-10-26 21:37:44 +0530

    Time: 1786 ms (20.31%), Space: 32.4 MB (9.42%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class Solution:
+    def __init__(self):
+        self.memory = defaultdict(list)
+        
+    def partition(self, s):
+        if not s: return [[]]
+        if s in self.memory: return self.memory[s]  # the memory trick can save some time
+        ans = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:  # prefix is a palindrome
+                for suf in self.partition(s[i:]):  # process suffix recursively
+                    ans.append([s[:i]] + suf)
+        self.memory[s] = ans
+        return ans
