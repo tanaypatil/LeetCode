f7491ede57fa71051339c4ce1e4be340f77d5c4b
tanaypatil <tanaypatil197@gmail.com>
2022-11-08 18:25:57 +0530

    Time: 383 ms (72.23%), Space: 14.1 MB (96.64%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,14 +1,33 @@
 class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
-        left, right = 0, 0
-        count = 0
-        freq = {}
-        for right, c in enumerate(s):
-            freq[c] = freq.get(c, 0) + 1
-            while len(freq) >= 3:
-                freq[s[left]] -= 1
-                if freq[s[left]] == 0:
-                    del freq[s[left]]
+    def atMostK(self, nums, K):
+        res = 0
+        left = 0
+        counts = Counter()
+        for right, num in enumerate(nums):
+            if not counts[num]:
+                K -= 1
+            counts[num] += 1
+            while K < 0:
+                counts[nums[left]] -= 1
+                if not counts[nums[left]]:
+                    K += 1
                 left += 1
-            count += left
-        return count
+            res += right-left+1
+        return res
+    
+    
+    def numberOfSubstrings(self, s: str) -> int:
+        # left, right = 0, 0
+        # count = 0
+        # freq = {}
+        # for right, c in enumerate(s):
+        #     freq[c] = freq.get(c, 0) + 1
+        #     while len(freq) >= 3:
+        #         freq[s[left]] -= 1
+        #         if freq[s[left]] == 0:
+        #             del freq[s[left]]
+        #         left += 1
+        #     count += left
+        # return count
+        
+        return self.atMostK(s, 3) - self.atMostK(s, 2)
