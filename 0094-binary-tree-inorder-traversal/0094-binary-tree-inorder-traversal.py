eb43f0f5f3ccc87b927cefa9ce7bbf08eb8e4201
tanaypatil <tanaypatil197@gmail.com>
2022-11-15 19:55:25 +0530

    Time: 58 ms (44.70%), Space: 13.9 MB (59.79%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -1,3 +1,5 @@
+from collections import deque, defaultdict
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -16,6 +18,27 @@ class Solution:
         self.iot(root.right)
     
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        Solution.arr = []
-        self.iot(root)
-        return Solution.arr
+        # Solution.arr = []
+        # self.iot(root)
+        # return Solution.arr
+        
+        if not root:
+            return []
+        
+        res = []
+        stack = deque()
+        stack.append(root)
+        vis = defaultdict(bool)
+        while stack:
+            r = stack.pop()
+            if vis[r]:
+                res.append(r.val)
+            else:
+                if r.right:
+                    stack.append(r.right)
+                stack.append(r)
+                if r.left:
+                    stack.append(r.left)
+                vis[r] = True
+        return res
+            
