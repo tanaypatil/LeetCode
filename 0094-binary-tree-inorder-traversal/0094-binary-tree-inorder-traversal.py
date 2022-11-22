7d1e373b2d044bccfa533bb39b26ed0bb1353c65
tanaypatil <tanaypatil197@gmail.com>
2022-11-22 17:01:31 +0530

    Time: 34 ms (89.87%), Space: 14 MB (14.03%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -16,11 +16,36 @@ class Solution:
         self.iot(root.left)
         Solution.arr.append(root.val)
         self.iot(root.right)
-    
+        
+    def morris(self, root):
+        if not root:
+            return []
+        inorder = []
+        
+        curr = root
+        while curr:
+            pre = curr.left
+            if not pre:
+                inorder.append(curr.val)
+                curr = curr.right
+            else:
+                while pre.right and pre.right != curr:
+                    pre = pre.right
+                if not pre.right:
+                    pre.right = curr
+                    curr = curr.left
+                else:
+                    pre.right = None
+                    inorder.append(curr.val)
+                    curr = curr.right
+        return inorder
+
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # Solution.arr = []
         # self.iot(root)
         # return Solution.arr
+        
+        return self.morris(root)
         
         if not root:
             return []
