9ca84f12bf825cbe6c84b3c149450039c5e33a67
tanaypatil <tanaypatil197@gmail.com>
2022-12-08 19:27:42 +0530

    Time: 269 ms (43.53%), Space: 14.5 MB (83.54%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,31 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        if not grid:
+            return 0
+        
+        heap = [(grid[0][0], 0, 0)]
+        
+        ma = float('-inf')
+        n = len(grid)
+        
+        moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        visited = defaultdict(bool)
+        visited[(0, 0)] = True
+        
+        while heap:
+            v, i, j = heappop(heap)
+            
+            if i == n-1 and j == n-1:
+                ma = max(ma, v)
+                return ma
+            
+            ma = max(ma, v)
+            for x, y in moves:
+                r, c = i+x, j+y
+                if 0 <= r < n and 0 <= c < n and not visited[(r, c)]:
+                    heappush(heap, (grid[r][c], r, c))
+                    visited[(r, c)] = True
+                    
+        return ma if ma != float('-inf') else -1
+        
