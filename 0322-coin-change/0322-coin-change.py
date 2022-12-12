da918787845ce9829102ee03fa0382488cc8de4e
tanaypatil <tanaypatil197@gmail.com>
2022-12-12 23:24:51 +0530

    Time: 7010 ms (5.00%), Space: 14.2 MB (62.56%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        if not coins:
+            return -1
+        dp = [0]*(amount+1)
+        for i in range(1, amount+1):
+            dp[i] = float('inf') if i%coins[0] else i//coins[0]
+        # print(dp)
+            
+        for i in range(1, len(coins)):
+            for j in range(1, amount+1):
+                not_taken = dp[j]
+                taken = float('inf')
+                if not j%coins[i]:
+                    taken = j//coins[i]
+                if j >= coins[i]:
+                    taken = min(taken, 1+dp[j-coins[i]])
+                dp[j] = min(taken, not_taken)
+            # print(dp)
+        
+        return -1 if dp[-1] == float('inf') else dp[-1]
