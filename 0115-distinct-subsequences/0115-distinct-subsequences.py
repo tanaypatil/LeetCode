084c8063fcd30e7f29dd3d5c21925f32600e098e
tanaypatil <tanaypatil197@gmail.com>
2022-12-14 17:45:53 +0530

    Time: 69 ms (98.72%), Space: 18.8 MB (82.37%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dfs(self, index, path):
+        if index >= len(Solution.s):
+            return 1 if path == Solution.t else 0
+        if len(path) >= len(Solution.t):
+            return 1 if path == Solution.t else 0
+        if len(Solution.s)-index + len(path) < len(Solution.t):
+            return 0
+        if (index, path) in Solution.mem:
+            return Solution.mem[(index, path)]
+        taken = 0
+        if Solution.s[index] == Solution.t[len(path)]:
+            taken += self.dfs(index+1, path+Solution.s[index])
+        not_taken =  self.dfs(index+1,  path)
+        
+        c = taken + not_taken
+        Solution.mem[(index, path)] = c
+        return c
+           
+        
+    def numDistinct(self, s: str, t: str) -> int:
+        Solution.mem = {}
+        Solution.s, Solution.t = s, t
+        return self.dfs(0, "")
+        
