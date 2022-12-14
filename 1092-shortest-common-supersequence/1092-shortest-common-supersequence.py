d8275827cb7b6f5ee5cabb9dd6997b0e993dfbbf
tanaypatil <tanaypatil197@gmail.com>
2022-12-14 19:22:26 +0530

    Time: 498 ms (86.46%), Space: 14 MB (100.00%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -0,0 +1,52 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        def lcs(str1, str2):
+            n, m = len(str1), len(str2)
+            prev = [""]*(m+1)
+            str2 = " "+str2
+            for i in range(n):
+                dp = [""]*(m+1)
+                for j in range(1, m+1):
+                    dp[j] = str1[i] + prev[j-1] if str1[i] == str2[j] else max(prev[j], dp[j-1], key=len)
+                prev = dp
+            return prev[-1]
+        
+        res, i, j = "", 0, 0
+        lc = lcs(str1, str2)
+        # print(lc)
+        for c in lc[::-1]:
+            while i < len(str1) and str1[i] != c:
+                res += str1[i]
+                i += 1
+            while j < len(str2) and str2[j] != c:
+                res += str2[j]
+                j += 1
+            res += c
+            i += 1
+            j += 1
+        return res + str1[i:] + str2[j:]
+    
+    
+        # def lcs(A, B):
+        #     n, m = len(A), len(B)
+        #     dp = [["" for _ in range(m + 1)] for _ in range(n + 1)]
+        #     for i in range(n):
+        #         for j in range(m):
+        #             if A[i] == B[j]:
+        #                 dp[i + 1][j + 1] = dp[i][j] + A[i]
+        #             else:
+        #                 dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1], key=len)
+        #     return dp[-1][-1]
+
+#         res, i, j = "", 0, 0
+#         print(lcs(A, B))
+#         for c in lcs(A, B):
+#             while A[i] != c:
+#                 res += A[i]
+#                 i += 1
+#             while B[j] != c:
+#                 res += B[j]
+#                 j += 1
+#             res += c
+#             i, j = i + 1, j + 1
+#         return res + A[i:] + B[j:]
