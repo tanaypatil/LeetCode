098bd27d7f82f17b11e42b6ab2c830b16a8a3893
tanaypatil <tanaypatil197@gmail.com>
2022-12-20 17:31:52 +0530

    Added solution - LeetHub

---
+++ Search Pattern (Z-algorithm) - GFG/search-pattern-zalgorithm.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def search(self, patt, s):
+        # code here
+        indexes = []
+        for i, c in enumerate(s):
+            if c == patt[0] and s[i:i+len(patt)] == patt:
+                indexes.append(i+1)
+        return indexes if indexes else [-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        patt = input().strip()
+        ob = Solution()
+        ans = ob.search(patt, s)
+        if len(ans)==0:
+            print("-1", end = " ")
+        else:
+            for value in ans:
+                print(value,end = ' ')
+        print()
+# } Driver Code Ends
