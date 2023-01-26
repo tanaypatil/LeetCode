5548be4c66af197208d503cd43ca2a748c9274fc
tanaypatil <tanaypatil197@gmail.com>
2023-01-26 19:10:39 +0530

    Time: 74 ms (25.83%), Space: 14 MB (68.63%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,26 +2,22 @@ from collections import Counter, defaultdict
 
 
 class Solution:
-    def find(self, s, wordDict, memo):
-        if not s:
+    def find(self, i):
+        if i >= len(Solution.s):
             return True
-        if s in memo and not memo[s]: return False
-        for word in wordDict:
-            if s[0] == word[0] and len(s) >= len(word) and s[:len(word)] == word:
-                if self.find(s[len(word):], wordDict, memo):
+        if i in Solution.mem:
+            return Solution.mem[i]
+        for word in Solution.wordDict:
+            if Solution.s[i] == word[0] and word == Solution.s[i:i+len(word)]:
+                if self.find(i+len(word)):
                     return True
-        memo[s] = False
+        Solution.mem[i] = False
         return False
     
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        counts = Counter(s)
-        word_counts = defaultdict(int)
-        for word in wordDict:
-            c = Counter(word)
-            for l in word:
-                word_counts[l] += 1
+        Solution.s = s
+        Solution.wordDict = wordDict
+        Solution.mem = {}
+        return self.find(0)
         
-        for k, v in counts.items():
-            if not word_counts[k]:
-                return False
-        return self.find(s, wordDict, {})
+        
