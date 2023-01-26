563daa04ac4569d934e65aa109a88ad5e8aae85d
tanaypatil <tanaypatil197@gmail.com>
2023-01-26 18:45:28 +0530

    Time: 106 ms (81.95%), Space: 14.2 MB (45.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,29 +1,12 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-#         def dfs(index, last, longest):
-#             if index >= len(nums):
-#                 return longest
-            
-#             not_taken = dfs(index+1, last, longest)
-#             taken = float('-inf')
-#             if not last or nums[index] > last:
-#                 taken = 1+dfs(index+1, nums[index], longest)
-#             return max(not_taken, taken)
-#         return dfs(0, None, 0)
-    
-        # n = len(nums)
-        # dp = [1]*n
-        # for i in range(n):
-        #     for j in range(i):
-        #         if nums[i] > nums[j]:
-        #             dp[i] = max(dp[i], 1+dp[j])
-        # return dp[-1]
-        sub = []
-        for x in nums:
-            if len(sub) == 0 or sub[-1] < x:
-                sub.append(x)
+        li = []
+        m = 0
+        for num in nums:
+            if li and li[-1] >= num:
+                index = bisect.bisect_left(li, num)
+                li[index] = num
             else:
-                idx = bisect_left(sub, x)  # Find the index of the first element >= x
-                sub[idx] = x  # Replace that number with x
-            # print(sub)
-        return len(sub)
+                li.append(num)
+                m = max(m, len(li))
+        return m
