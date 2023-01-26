1a1cc6907b41981e0345527e8a93fe9440c9ef10
tanaypatil <tanaypatil197@gmail.com>
2023-01-26 18:29:44 +0530

    Time: 60 ms (14.06%), Space: 13.9 MB (53.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,14 @@
-def numClimbsRec(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    return numClimbs(n-1) + numClimbs(n-2)
 
-def numClimbsItr(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    arr = [0]*n
-    arr[0], arr[1] = 1, 2
-    p = 0
-    for i in range(2, n):
-        arr[i] = arr[i-1] + arr[i-2]
-        p = arr[i]
-    return p
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return numClimbsItr(n)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        dp = [0, 1, 2]
+        for i in range(3, n+1):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[-1]
         
