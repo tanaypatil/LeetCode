3cbb73294ee7e5080d35f7335351edd5f5c291df
tanaypatil <tanaypatil197@gmail.com>
2023-02-23 23:12:54 +0530

    Time: 248 ms (66.31%), Space: 14 MB (73.41%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,14 +2,8 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0]*(amount+1)
         dp[0] = 1
-        
-        for i in range(len(coins)):
+        for c in coins:
             for j in range(1, amount+1):
-                not_taken = dp[j]
-                taken = 0
-                if j >= coins[i]:
-                    taken += dp[j-coins[i]]
-                dp[j] = taken + not_taken
-        
+                dp[j] = dp[j] + dp[j-c] if j >= c else dp[j]
         return dp[-1]
         
