c44ef22c64c0dc787187fb5f341356c76e65498c
tanaypatil <tanaypatil197@gmail.com>
2023-04-18 13:16:33 +0530

    Time: 837 ms (52.13%), Space: 17.9 MB (69.42%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -2,7 +2,6 @@ class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         change = {5: 0, 10: 0, 20: 0}
         for bill in bills:
-            # print(bill, change)
             if bill == 20:
                 if change[5] >= 1 and change[10] >= 1:
                     change[5] -= 1
