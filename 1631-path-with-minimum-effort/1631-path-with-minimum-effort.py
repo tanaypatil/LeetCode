1c43aa413b047683ab1f79dbfc4da4ccfa306466
tanaypatil <tanaypatil197@gmail.com>
2023-04-20 00:16:49 +0530

    Time: 681 ms (92.78%), Space: 15.2 MB (86.70%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -6,14 +6,10 @@ class Solution:
         if rows == 1 and cols == 1:
             return 0
         
-        # q = deque({(0, 0, -1)})
-        # visited = defaultdict(bool)
-        
         heap = [(0, 0, 0)]
         
         r = [-1, 1, 0, 0]
         c = [0, 0, -1, 1]
-        # ans = float('inf')
         dist = [[float('inf')]*cols for _ in range(rows)]
         while heap:
             eff, row, col = heappop(heap)
@@ -28,16 +24,3 @@ class Solution:
                         heappush(heap, (diff, x, y))
                         
         return -1
-            
-            
-#             s = len(q)
-#             for _ in range(s):
-#                 row, col, eff = q.popleft()
-#                 if row == rows-1 and col == cols-1:
-#                     ans = min(ans, eff)
-#                     continue
-#                 if visited[(row, col, eff)]: continue
-#                 if eff >= ans: continue
-#                 visited[(row, col, eff)] = True
-                
-#         return -1 if ans == float('inf') else ans
