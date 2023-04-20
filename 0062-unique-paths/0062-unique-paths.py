da42ba7e5b2aae427ee449936a8e4883047b6500
tanaypatil <tanaypatil197@gmail.com>
2023-04-20 09:57:59 +0530

    Time: 37 ms (28.05%), Space: 13.8 MB (61.10%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,20 +1,12 @@
 class Solution:
-    def dfs(self, m, n, r, c):
-        if r == m-1 and c == n-1:
-            return 1
-        if (r, c) in Solution.mem:
-            return Solution.mem[(r, c)]
-        ans = 0
-        for di, dj in Solution.moves:
-            i, j = r+di, c + dj
-            if 0 <= i < m and 0 <= j < n:
-                ans += self.dfs(m, n, i, j)
-        Solution.mem[(r, c)] = ans
-        return ans
         
     def uniquePaths(self, m: int, n: int) -> int:
-        Solution.mem = {}
-        Solution.moves = [(1, 0), (0, 1)]
-        return self.dfs(m, n, 0, 0)
+        dp = [[1]*n for _ in range(m)]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                
+        return dp[-1][-1]
         
             
