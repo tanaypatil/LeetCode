e5f4dd9f18f757c0107fac4b07402d863d87cab6
tanaypatil <tanaypatil197@gmail.com>
2023-04-22 00:32:42 +0530

    Time: 8588 ms (5.01%), Space: 30 MB (6.55%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        d = collections.defaultdict(set)
+        for u, v in edges:
+            d[u].add(v)
+            d[v].add(u)
+        s = set(range(n))
+        while len(s) > 2:
+            leaves = set(i for i in s if len(d[i]) == 1)
+            s -= leaves
+            for i in leaves:
+                for j in d[i]:
+                    d[j].remove(i)
+        return list(s)
+                
