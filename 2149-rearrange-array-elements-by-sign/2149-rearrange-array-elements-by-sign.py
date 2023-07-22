447e30e0653539adbd8a08ca4cbab8a6dbaa55f1
tanaypatil <tanaypatil197@gmail.com>
2023-07-22 13:07:36 +0530

    Time: 1465 ms (74.43%), Space: 47.9 MB (12.92%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -7,22 +7,9 @@ class Solution:
                 pos.append(num)
             else:
                 neg.append(num)
-        i = j = 0
         ret = []
-        c = 0
-        while i < len(pos) and j < len(neg):
-            if c & 1:
-                ret.append(neg[j])
-                j += 1
-            else:
-                ret.append(pos[i])
-                i += 1
-            c += 1
-        while i < len(pos):
+        for i in range(len(pos)):
             ret.append(pos[i])
-            i += 1
-        while j < len(neg):
-            ret.append(neg[j])
-            j += 1
+            ret.append(neg[i])
         return ret
         
