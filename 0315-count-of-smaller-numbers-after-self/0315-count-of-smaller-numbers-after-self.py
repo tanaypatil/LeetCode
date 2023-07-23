4694b7e54bc0f73dcca45752a5a1b8bc47c7f746
tanaypatil <tanaypatil197@gmail.com>
2023-07-23 23:29:26 +0530

    Time: 2690 ms (77.25%), Space: 44.7 MB (25.55%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,9 +1,30 @@
 class Solution:
+    def merge(self, left, right):
+        merged = []
+        while left and right:
+            if left[-1][1] > right[-1][1]:
+                Solution.res[left[-1][0]] += len(right)
+                merged.append(left.pop())
+            else:
+                merged.append(right.pop())
+        
+        while left:
+            merged.append(left.pop())
+        
+        while right:
+            merged.append(right.pop())
+        return merged
+        
+    def merge_sort(self, nums):
+        if len(nums) == 1:
+            return nums
+        m = len(nums)//2
+        left = self.merge_sort(nums[:m])
+        right = self.merge_sort(nums[m:])
+        return self.merge(left, right)[::-1]
+
     def countSmaller(self, nums: List[int]) -> List[int]:
-        res = [0]*len(nums)
-        sorted_rev = [nums[-1]]
-        for i in range(len(nums)-2, -1, -1):
-            index = bisect_left(sorted_rev, nums[i])
-            res[i] += index
-            sorted_rev.insert(index, nums[i])
-        return res
+        index_nums = [*enumerate(nums)]
+        Solution.res = [0]*len(nums)
+        self.merge_sort(index_nums)
+        return Solution.res
