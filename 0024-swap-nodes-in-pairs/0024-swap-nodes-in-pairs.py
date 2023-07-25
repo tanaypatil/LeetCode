f06a29f9f52e2d29dc17f0336820386e3345517a
tanaypatil <tanaypatil197@gmail.com>
2023-07-25 17:45:13 +0530

    Time: 57 ms (6.40%), Space: 16.4 MB (46.57%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseK(self, head, n, k):
+        if not head or not head.next or n < k:
+            return head
+        t = head
+        for i in range(k-1):
+            t = t.next
+        nex = t.next
+        nexl = self.reverseK(nex, n-k, k)
+        t.next = None
+        
+        p = None
+        t = head
+        
+        while t:
+            nex = t.next
+            t.next = p
+            p = t
+            t = nex
+        
+        head.next = nexl
+        return p
+    
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        l = 0
+        t = head
+        while t:
+            l += 1
+            t = t.next
+        return self. reverseK(head, l, 2)
