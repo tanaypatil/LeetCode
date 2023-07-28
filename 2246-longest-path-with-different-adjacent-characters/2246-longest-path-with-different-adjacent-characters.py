773d857bd54be8b47e611bd6bdf8a198d60efa93
tanaypatil <tanaypatil197@gmail.com>
2023-07-29 01:30:05 +0530

    Time: 1822 ms (45.81%), Space: 171.9 MB (13.17%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,10 +1,10 @@
 class Solution:
-    def dfs(self, i, s, adj, visited):
+    def dfs(self, i, s, adj, visited, max_path):
         visited[i] = True
         children = ['', '']
         for j in adj[i]:
             if not visited[j]:
-                child_path = self.dfs(j, s, adj, visited)
+                child_path = self.dfs(j, s, adj, visited, max_path)
                 if child_path[-1] != s[i]:
                     if len(child_path) > len(children[0]):
                         children[1] = children[0]
@@ -12,7 +12,7 @@ class Solution:
                     elif len(child_path) > len(children[1]):
                         children[1] = child_path
         
-        Solution.max_path = max(Solution.max_path, len(children[0]+children[1])+1)
+        max_path[0] = max(max_path[0], len(children[0]+children[1])+1)
         
         return children[0]+s[i]
                 
@@ -20,16 +20,12 @@ class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
         adj = defaultdict(list)
         visited = defaultdict(bool)
-        
         for u, v in enumerate(parent):
             if u != 0:
                 adj[u].append(v)
                 adj[v].append(u)
-            
-        Solution.max_path = 0
-        
-        self.dfs(0, s, adj, visited)
-        
-        return Solution.max_path
+        max_path = [0]
+        self.dfs(0, s, adj, visited, max_path)
+        return max_path[0]
         
         
