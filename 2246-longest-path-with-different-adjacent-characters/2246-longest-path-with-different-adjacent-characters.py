c41e1cae5bcb8173936446e290ac18027034be98
tanaypatil <tanaypatil197@gmail.com>
2023-07-29 01:24:26 +0530

    Time: 1917 ms (31.74%), Space: 167.7 MB (20.06%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -0,0 +1,38 @@
+class Solution:
+    def dfs(self, i):
+        Solution.visited[i] = True
+        children = ['', '']
+        for j in Solution.adj[i]:
+            if not Solution.visited[j]:
+                child_path = self.dfs(j)
+                if child_path[-1] != Solution.word[i]:
+                    if len(child_path) > len(children[0]):
+                        children[1] = children[0]
+                        children[0] = child_path
+                    elif len(child_path) > len(children[1]):
+                        children[1] = child_path
+        
+        pre, post = children
+        Solution.max_path = max(Solution.max_path, len(pre+post)+1)
+        
+        return pre+Solution.word[i]
+                
+        
+    def longestPath(self, parent: List[int], s: str) -> int:
+        Solution.word = s
+        adj = defaultdict(list)
+        Solution.visited = defaultdict(bool)
+        
+        for u, v in enumerate(parent):
+            if u != 0:
+                adj[u].append(v)
+                adj[v].append(u)
+            
+        Solution.adj = adj
+        Solution.max_path = 0
+        
+        self.dfs(0)
+        
+        return Solution.max_path
+        
+        
