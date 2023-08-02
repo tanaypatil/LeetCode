bc66b96d04bdc059910dfca0858cd78d61ac33a4
tanaypatil <tanaypatil197@gmail.com>
2023-08-02 17:57:24 +0530

    Time: 110 ms (98.01%), Space: 17.3 MB (56.57%) - LeetHub

---
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        closingPCount = 0
+        stack = deque()
+        invalid = 0
+        for c in s:
+            if c == "(":
+                if closingPCount == 1:
+                    invalid += 1
+                    closingPCount = 0
+                    if stack:
+                        stack.pop()
+                    else:
+                        invalid += 1
+                stack.append(c)
+            else:
+                if closingPCount == 0:
+                    closingPCount += 1
+                else:
+                    if stack:
+                        stack.pop()
+                    else:
+                        invalid += 1
+                    closingPCount = 0
+            # print(stack, closingPCount)
+        if closingPCount == 1:
+            invalid += 1
+            if stack:
+                stack.pop()
+            else:
+                invalid += 1
+        return invalid + len(stack)*2
+                    
+        
