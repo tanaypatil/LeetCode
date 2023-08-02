1046cf46b2077d14b5cbca2776de412e72350e73
tanaypatil <tanaypatil197@gmail.com>
2023-08-02 19:56:40 +0530

    Time: 2295 ms (27.50%), Space: 143.7 MB (38.75%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[str]]) -> bool:
+        
+        @cache
+        def dfs(i, j, stack):
+            if i == len(grid)-1 and j == len(grid[0])-1:
+                return len(stack) == 1 and grid[i][j] == ")"
+            
+            if grid[i][j] == "(":
+                for di, dj in [(1, 0), (0, 1)]:
+                    r, c = i + di, j + dj
+                    if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
+                        if dfs(r, c, stack+"("): return True
+            else:
+                if stack:
+                    for di, dj in [(1, 0), (0, 1)]:
+                        r, c = i + di, j + dj
+                        if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
+                            if dfs(r, c, stack[:-1]): return True
+            return False
+        
+        return dfs(0, 0, "")
+        
