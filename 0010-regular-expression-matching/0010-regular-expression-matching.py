645cd0243dd57ceac846c37a10ec1a74682f0f7f
tanaypatil <tanaypatil197@gmail.com>
2023-08-04 01:02:07 +0530

    Time: 52 ms (84.66%), Space: 16.4 MB (78.78%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,20 +1,24 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
-        dp = [[False]*(m+1) for _ in range(n+1)]
+        p, s = " " + p, " " + s
+        
         prev = [[False]*(m+1) for _ in range(2)]
         prev[1][0] = True
-        p = " " + p
-        s = " " + s
-        dp[0][0] = True
+        
         for i in range(1, n+1):
+            
             dp = [False]*(m+1)
             dp[0] = prev[-1][0] or prev[-2][0] if p[i] == "*" else False
+            
             for j in range(1, m+1):
+                
                 if p[i] == s[j] or p[i] == ".":
                     dp[j] = prev[-1][j-1]
                 elif p[i] == "*":
                     dp[j] = prev[-1][j] or prev[-2][j] or ((prev[-1][j-1] or dp[j-1]) and (s[j] == p[i-1] or p[i-1] == "."))
+                    
             prev[0] = prev[1]
             prev[1] = dp
+        
         return prev[-1][-1]
