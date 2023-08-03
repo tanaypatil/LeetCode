c06ecfb662d27061e45fe51a3a2e3c958a8898ce
tanaypatil <tanaypatil197@gmail.com>
2023-08-04 01:00:53 +0530

    Time: 52 ms (84.66%), Space: 16.4 MB (78.78%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,14 +2,19 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
         dp = [[False]*(m+1) for _ in range(n+1)]
+        prev = [[False]*(m+1) for _ in range(2)]
+        prev[1][0] = True
         p = " " + p
         s = " " + s
         dp[0][0] = True
         for i in range(1, n+1):
-            dp[i][0] = dp[i-1][0] or dp[i-2][0] if p[i] == "*" else False
+            dp = [False]*(m+1)
+            dp[0] = prev[-1][0] or prev[-2][0] if p[i] == "*" else False
             for j in range(1, m+1):
                 if p[i] == s[j] or p[i] == ".":
-                    dp[i][j] = dp[i-1][j-1]
+                    dp[j] = prev[-1][j-1]
                 elif p[i] == "*":
-                    dp[i][j] = dp[i-1][j] or dp[i-2][j] or ((dp[i-1][j-1] or dp[i][j-1]) and (s[j] == p[i-1] or p[i-1] == "."))
-        return dp[-1][-1]
+                    dp[j] = prev[-1][j] or prev[-2][j] or ((prev[-1][j-1] or dp[j-1]) and (s[j] == p[i-1] or p[i-1] == "."))
+            prev[0] = prev[1]
+            prev[1] = dp
+        return prev[-1][-1]
