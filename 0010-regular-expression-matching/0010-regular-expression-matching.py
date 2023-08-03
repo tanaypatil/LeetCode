f43e0a09dba49d97a5b734ba64b570284a27a81f
tanaypatil <tanaypatil197@gmail.com>
2023-08-04 01:05:12 +0530

    Time: 50 ms (88.40%), Space: 16.3 MB (78.57%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -16,7 +16,9 @@ class Solution:
                 if p[i] == s[j] or p[i] == ".":
                     dp[j] = prev[-1][j-1]
                 elif p[i] == "*":
-                    dp[j] = prev[-1][j] or prev[-2][j] or ((prev[-1][j-1] or dp[j-1]) and (s[j] == p[i-1] or p[i-1] == "."))
+                    dp[j] = prev[-1][j] or prev[-2][j]
+                    if (s[j] == p[i-1] or p[i-1] == "."):
+                        dp[j] = dp[j] or prev[-1][j-1] or dp[j-1]
                     
             prev[0] = prev[1]
             prev[1] = dp
