4f3278a9ccc2ba8755d3e166a4ea850df9cfe926
tanaypatil <tanaypatil197@gmail.com>
2023-08-04 00:48:08 +0530

    Time: 56 ms (73.09%), Space: 16.4 MB (56.49%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -5,7 +5,6 @@ class Solution:
         p = " " + p
         s = " " + s
         dp[0][0] = True
-        
         for i in range(1, n+1):
             dp[i][0] = dp[i-1][0] or dp[i-2][0] if p[i] == "*" else False
             for j in range(1, m+1):
