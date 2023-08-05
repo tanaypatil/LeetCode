ca59dfe3acccbdda0e538caa1a96ad9b208e14de
tanaypatil <tanaypatil197@gmail.com>
2023-08-06 01:27:10 +0530

    Time: 301 ms (85.05%), Space: 43.2 MB (34.58%) - LeetHub

---
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numberWays(self, hats: List[List[int]]) -> int:
+        n = len(hats)
+        people = defaultdict(list)
+        for i in range(n):
+            for hat in hats[i]:
+                people[hat-1].append(i)
+        MOD = 10**9+7
+        
+        if n > 41: return 0
+        
+        @lru_cache(None)
+        def dp(i, mask):
+            if mask == (1<<n)-1:
+                return 1
+            if i == 40: return 0
+            cur = 0
+            for j in people[i]:
+                if not (mask & (1 << j)):
+                    cur += dp(i+1, mask|1<<j)
+            cur += dp(i+1, mask)
+            return cur%MOD
+        return dp(0, 0)
+        
