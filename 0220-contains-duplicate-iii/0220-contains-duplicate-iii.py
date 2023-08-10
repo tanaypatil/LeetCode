713fc3118c5a1e4ca097fdd89d1c3cc9b5944397
tanaypatil <tanaypatil197@gmail.com>
2023-08-11 00:03:38 +0530

    Time: 3081 ms (5.06%), Space: 38.8 MB (18.41%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -0,0 +1,50 @@
+class Solution:
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        mid = len(arr)//2
+        left_sorted = self.merge_sort(arr[:mid])
+        right_sorted = self.merge_sort(arr[mid:])
+        
+        l, r = 0, 0
+        # print(left_sorted, right_sorted)
+        while l < len(left_sorted) and r < len(right_sorted):
+            if abs(left_sorted[l][1] - right_sorted[r][1]) <= self.valueDiff:
+                if right_sorted[r][0] - left_sorted[l][0] <= self.indexDiff:
+                    self.ans = True
+                    break
+                else:
+                    l += 1
+            else:
+                if left_sorted[l][1] < right_sorted[r][1]:
+                    l += 1
+                else:
+                    r += 1
+        
+        l, r = 0, 0
+        final_sorted = []
+        while l < len(left_sorted) and r < len(right_sorted):
+            if left_sorted[l][1] < right_sorted[r][1]:
+                final_sorted.append(left_sorted[l])
+                l += 1
+            else:
+                final_sorted.append(right_sorted[r])
+                r += 1
+                
+        while l < len(left_sorted):
+            final_sorted.append(left_sorted[l])
+            l += 1
+            
+        while r < len(right_sorted):
+            final_sorted.append(right_sorted[r])
+            r += 1
+            
+        return final_sorted
+        
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        n = len(nums)
+        self.ans = False
+        self.valueDiff = valueDiff
+        self.indexDiff = indexDiff
+        self.merge_sort([*enumerate(nums)])
+        return self.ans
