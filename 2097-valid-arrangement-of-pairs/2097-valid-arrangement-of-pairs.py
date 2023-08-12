d1b70af66d2cc20e5ad45a5633f554b01cc6cd8c
tanaypatil <tanaypatil197@gmail.com>
2023-08-12 19:31:08 +0530

    Time: 2191 ms (92.75%), Space: 171.8 MB (69.56%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
@@ -1,20 +1,24 @@
 class Solution:
-    def validArrangement(self, pairs):
-        adj = defaultdict(list)
-        deg = Counter()
-        for u, v in pairs:
-            adj[u].append(v)
-            deg[u] += 1
-            deg[v] -= 1
-        
-        for start in adj:
-            if deg[start] == 1:
-                break
-
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        degree = defaultdict(int) # net out degree 
+        for x, y in pairs: 
+            graph[x].append(y)
+            degree[x] += 1
+            degree[y] -= 1
+                
+        for k in degree: 
+            if degree[k] == 1: 
+                x = k
+                break 
+                
         ans = []
-        def dfs(node):
-            while adj[node]:
-                dfs(nei := adj[node].pop())
-                ans.append([node, nei])
-        dfs(start)
-        return ans[::-1]
+
+        def fn(x): 
+            """Return Eulerian path via dfs."""
+            while graph[x]: fn(graph[x].pop()) 
+            ans.append(x)
+        
+        fn(x)
+        ans.reverse()
+        return [[ans[i], ans[i+1]] for i in range(len(ans)-1)]
