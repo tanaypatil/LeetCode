e80fb26f16066fa72360f0b10597520703b27409
tanaypatil <tanaypatil197@gmail.com>
2023-08-12 23:24:51 +0530

    Time: 33 ms (94.08%), Space: 16.2 MB (85.36%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, words: List[str]) -> List[str]:
+        visited = defaultdict(bool)
+        ans = []
+        def dp(s, path):
+            if not s:
+                ans.append(' '.join(path))
+                return
+            for word in words:
+                if len(word) <= len(s) and s[:len(word)] == word:
+                    dp(s[len(word):], path+[word])
+        dp(s, [])
+        return ans
+                
