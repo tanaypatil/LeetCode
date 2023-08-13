37cfe9757a2bc4117abdf6c5115e1fa4e25e3c72
tanaypatil <tanaypatil197@gmail.com>
2023-08-13 16:40:08 +0530

    Time: 2352 ms (5.04%), Space: 36.3 MB (6.59%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -6,7 +6,7 @@ class Solution:
         for i, num in enumerate(nums):
             cum_sum += num
             p = max(bisect_left(min_sums, (cum_sum-k, -1))-1, 0)
-            while 0 <= p < len(min_sums) and cum_sum - min_sums[p][0] >= k:
+            while p < len(min_sums) and cum_sum - min_sums[p][0] >= k:
                 ans = min(ans, i-min_sums[p][1])
                 p += 1
             while min_sums and cum_sum <= min_sums[-1][0]:
