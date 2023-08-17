9a121696af6fc17521dc4eaf5aa53ad6e0b990de
tanaypatil <tanaypatil197@gmail.com>
2023-08-17 23:49:09 +0530

    Time: 3990 ms (48.75%), Space: 30 MB (24.38%) - LeetHub

--- 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.py
+++ 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.py
@@ -1,43 +1,39 @@
-class Node(object):
+class Node:
     def __init__(self):
-        self.children = {}  # child of node
-        self.cnt = 0        # cnt the nums having this bit
-
-class Trie:
-    def __init__(self):
-        self.root = Node()
-
-    def insert(self, val):
-        curr = self.root
-        for i in range(31, -1, -1):
-            bit = bool(val & (1<<i)) # or (val>>i) & 1
+        self.children = {}
+        self.count = 0
+        
+    def insert(self, num):
+        curr = self
+        for i in range(15, -1, -1):
+            bit = (num >> i) & 1
             if bit not in curr.children:
-                curr.children[bit] = Node()   # if no node of bit make node of bit
-            curr = curr.children[bit]     # go to bit node
-            curr.cnt += 1   # increment the cnt of node
-
-    def cntSmallXorQuery(self, val,high):  # cnt the nums having xor with val < high
+                curr.children[bit] = Node()
+            curr = curr.children[bit]
+            curr.count += 1
+    
+    def limit_xor(self, num, limit):
+        curr = self
         res = 0
-        curr = self.root
-        for i in range(31, -1, -1):
-            if not curr :
+        for i in range(15, -1, -1):
+            if not curr:
                 break
-            bit = (val>>i) & 1
-            cmp = (high>>i) & 1 
-            if cmp :  # if set 1
-                res += curr.children.get(bit,Node()).cnt   # cnt the nums having this bit
-                curr = curr.children.get(1-bit,None)   # move to opposite bit
-            else: 
-                curr = curr.children.get(bit,None)     # move to the same bit
+            bit = (num >> i) & 1
+            l = (limit >> i) & 1
+            if l:
+                res += curr.children.get(bit, Node()).count
+                curr = curr.children.get(1^bit, None)
+            else:
+                curr = curr.children.get(bit, None)
         return res
-
+    
 
 
 class Solution:
     def countPairs(self, nums: List[int], low: int, high: int) -> int:
-        trie = Trie()
-        res = 0
-        for num in nums: 
-            res += trie.cntSmallXorQuery(num, high+1) - trie.cntSmallXorQuery(num, low)
+        trie = Node()
+        ans = 0
+        for num in nums:
+            ans += trie.limit_xor(num, high+1) - trie.limit_xor(num, low)
             trie.insert(num)
-        return res
+        return ans
