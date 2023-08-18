8d47d982accf18cfe7133e472950a44f3077c56d
tanaypatil <tanaypatil197@gmail.com>
2023-08-19 01:39:58 +0530

    Time: 41 ms (60.63%), Space: 16.3 MB (39.97%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,38 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # print(s)
+        ans = ""
+        if not s: return ans
+        i = 0
+        d = ""
+        for i, c in enumerate(s):
+            if not c.isnumeric():
+                ans += c
+            else:
+                j = i
+                while s[j].isnumeric():
+                    d += s[j]
+                    j += 1
+                i = j-1
+                break
+        # print("d", d)
+        d = int(d) if d else 1
+        # print(d)
+        # print(i)
+        if i == len(s) - 1: return ans
+        stack = deque()
+        stack.append(s[i+1])
+        i += 2
+        r = "["
+        while stack and i < len(s):
+            if s[i] == "[":
+                stack.append("[")
+            elif s[i] == "]":
+                stack.pop()
+            r += s[i]
+            i += 1
+        # print(r)
+        ans += self.decodeString(r[1:-1])*d
+        ans += self.decodeString(s[i:])
+        return ans
+        
