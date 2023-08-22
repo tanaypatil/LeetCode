487d535a8c76a1b0f6b3b802bc5c59a254f16ab1
tanaypatil <tanaypatil197@gmail.com>
2023-08-22 23:59:54 +0530

    Time: 996 ms (78.36%), Space: 37.9 MB (24.80%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        s = sum(nums)-x
+        if not s:
+            return len(nums)
+        cs = 0
+        smap = {}
+        max_len = 0
+        for i, num in enumerate(nums):
+            # print(i, num)
+            cs += num
+            # print(cs, smap)
+            if cs == s:
+                max_len = i+1
+            elif num == s:
+                max_len = max(max_len, 1)
+            else:
+                if cs-s in smap:
+                    max_len = max(max_len, i-smap[cs-s])
+            if cs not in smap:
+                smap[cs] = i
+            # print(max_len)
+            # print("------")
+        if not max_len or max_len == len(nums):
+            return -1
+        
+        return len(nums)-max_len
+        
+        
