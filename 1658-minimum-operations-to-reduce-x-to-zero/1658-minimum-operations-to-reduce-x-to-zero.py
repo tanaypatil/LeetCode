96e7804b0e08f16ae7f2902ec4126f97f4f8fe14
tanaypatil <tanaypatil197@gmail.com>
2023-08-23 00:00:50 +0530

    Time: 985 ms (81.53%), Space: 38 MB (21.64%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -7,20 +7,15 @@ class Solution:
         smap = {}
         max_len = 0
         for i, num in enumerate(nums):
-            # print(i, num)
             cs += num
-            # print(cs, smap)
             if cs == s:
                 max_len = i+1
             elif num == s:
                 max_len = max(max_len, 1)
-            else:
-                if cs-s in smap:
-                    max_len = max(max_len, i-smap[cs-s])
+            elif cs-s in smap:
+                max_len = max(max_len, i-smap[cs-s])
             if cs not in smap:
                 smap[cs] = i
-            # print(max_len)
-            # print("------")
         if not max_len or max_len == len(nums):
             return -1
         
