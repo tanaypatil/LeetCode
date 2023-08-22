13143badff80c77f8e1ff716f51cf471990c9ead
tanaypatil <tanaypatil197@gmail.com>
2023-08-22 18:25:16 +0530

    Time: 3902 ms (7.28%), Space: 16.3 MB (53.27%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,40 @@
+class Solution:
+    def nextPermutation(self, digits: List[str]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pre = digits[:]
+        i = len(digits) - 1
+        while i-1 >= 0 and digits[i] <= digits[i-1]:
+            i -= 1
+            
+        if i == 0:
+            digits[:] = digits[::-1]
+            return
+        
+        j = i
+        while j+1 < len(digits) and digits[j+1] > digits[i-1]:
+            j += 1
+        
+        digits[i-1], digits[j] = digits[j], digits[i-1]
+        digits[i:] = digits[i:][::-1]
+        
+    def factorial(self, n):
+        if n == 1:
+            return 1
+        return n * self.factorial(n-1)
+        
+    def getPermutation(self, n: int, k: int) -> str:
+        s = []
+        for i in range(1, n+1):
+            s.append(str(i))
+            
+        for i in range(k-1):
+            next_perm = s[:]
+            self.nextPermutation(next_perm)
+            s = next_perm
+            
+        return ''.join(s)
+            
+        
+        
