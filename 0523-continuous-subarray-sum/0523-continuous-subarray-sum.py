eb282c6f5f7869881ba3c32d8ccd9ccc9edbd72c
tanaypatil <tanaypatil197@gmail.com>
2023-09-06 01:38:37 +0530

    Time: 963 ms (42.76%), Space: 36 MB (90.80%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,34 +1,35 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        # s = 0
-        # smap = {}
-        # zeroes = 0
-        # for i, num in enumerate(nums):
-        #     s += num
-        #     if i > 0 and not s % k:
-        #         return True
-        #     if (s % k in smap and i-smap[s%k] >=2): return True
-        #     smap[s%k] = i
-        # return False
-    
-        prefix_sum = 0
-        remainder_dict = {}
-
-        for i in range(len(nums)):
-
-            prefix_sum += nums[i]
-
-            if i >= 1 and prefix_sum % k == 0:
+        s = 0
+        smap = {}
+        zeroes = 0
+        for i, num in enumerate(nums):
+            s += num
+            if i > 0 and not s % k:
                 return True
+            if (s % k) in smap:
+                if i-smap[s%k] >= 2: return True
+            else: smap[s%k] = i
+        return False
+    
+#         prefix_sum = 0
+#         remainder_dict = {}
 
-            remainder = prefix_sum % k
+#         for i in range(len(nums)):
 
-            if remainder in remainder_dict:
-                if i - remainder_dict[remainder] >= 2:
-                    return True
+#             prefix_sum += nums[i]
+
+#             if i >= 1 and prefix_sum % k == 0:
+#                 return True
+
+#             remainder = prefix_sum % k
+
+#             if remainder in remainder_dict:
+#                 if i - remainder_dict[remainder] >= 2:
+#                     return True
                 
-            else:
-                remainder_dict[remainder] = i
+#             else:
+#                 remainder_dict[remainder] = i
 
         return False
                 
