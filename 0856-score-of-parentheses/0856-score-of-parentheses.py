b07c6853e90fcae73a12cd87c3cbfe70fbe10678
tanaypatil <tanaypatil197@gmail.com>
2023-09-10 00:21:52 +0530

    Time: 37 ms (69.77%), Space: 16.3 MB (66.22%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        stack = deque()
+        score = 1
+        ans = 0
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            else:
+                j = stack.pop()
+                if not stack:
+                    # print(s, j, i)
+                    score *= 2*self.scoreOfParentheses(s[j+1:i])
+                    if not score: score = 1
+                    ans += score
+                    # print(ans)
+                    score = 1
+        print(s, ans)
+        return ans
+                    
+        
