48661157a8dd29faa560b30d03f62f77b53bcb18
tanaypatil <tanaypatil197@gmail.com>
2023-09-10 00:22:17 +0530

    Time: 30 ms (95.52%), Space: 16.1 MB (92.89%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -11,13 +11,10 @@ class Solution:
             else:
                 j = stack.pop()
                 if not stack:
-                    # print(s, j, i)
                     score *= 2*self.scoreOfParentheses(s[j+1:i])
                     if not score: score = 1
                     ans += score
-                    # print(ans)
                     score = 1
-        # print(s, ans)
         return ans
                     
         
