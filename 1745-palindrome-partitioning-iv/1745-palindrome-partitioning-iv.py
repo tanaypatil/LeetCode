11810c99115305b304be2f4b3001ec31636c9087
tanaypatil <tanaypatil197@gmail.com>
2023-09-10 23:22:30 +0530

    Time: 4770 ms (32.89%), Space: 17.8 MB (71.14%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkPartitioning(self, s: str) -> bool:
+        n = len(s)
+        
+        @lru_cache(None)
+        def dfs(i, p):
+            if not p and i >= n:
+                return True
+            
+            if not p or i >= n:
+                return False
+            
+            
+            for j in range(i, n):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    if dfs(j+1, p-1):
+                        return True
+            return False
+        
+        return dfs(0, 3)
