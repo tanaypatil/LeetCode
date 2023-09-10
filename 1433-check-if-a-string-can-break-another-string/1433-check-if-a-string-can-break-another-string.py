4184443a06e4a90d6f316b3c7bf76f4fb2257fc3
tanaypatil <tanaypatil197@gmail.com>
2023-09-10 23:11:50 +0530

    Time: 214 ms (13.24%), Space: 18.9 MB (68.21%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
@@ -8,22 +8,6 @@ class Solution:
             if c1 < c2:
                 return False
         return True
-        
-#         @lru_cache(None)
-#         def dfs(i, j):
-#             if i >= n:
-#                 return True
-            
-#             if j >= n:
-#                 return False
-            
-#             for k in range(j, n):
-#                 if s2[k] >= s1[i]:
-#                     if dfs(i+1, k+1):
-#                         return True
-#             return False
-        
-#         return dfs(0, 0)
     
     def checkIfCanBreak(self, s1: str, s2: str) -> bool:
         return self.check(s1, s2) or self.check(s2, s1)
