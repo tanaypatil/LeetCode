b8754c5a89ec819e440efb00ec9ca6fdf464e5e7
tanaypatil <tanaypatil197@gmail.com>
2023-09-14 23:45:27 +0530

    Time: 450 ms (5.69%), Space: 128.5 MB (5.35%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
@@ -4,13 +4,10 @@ class Solution:
         
         @lru_cache(None)
         def dp(i, k, curr_count, curr_sum):
-            if i >= n:
-                return 0
-            if not k:
-                return float('-inf')
-            curr_avg = (curr_sum + nums[i])/(curr_count + 1)
-            return max(curr_avg + dp(i+1, k-1, 0, 0), dp(i+1, k, (curr_count + 1), (curr_sum + nums[i])))
-        
+            if i >= n: return 0
+            if not k: return float('-inf')
+            return max((curr_sum + nums[i])/(curr_count + 1) + dp(i+1, k-1, 0, 0), dp(i+1, k, (curr_count + 1), (curr_sum + nums[i])))
+    
         return dp(0, k, 0, 0)
             
             
