fbb1144c0f2e66504936ae49e7b2d643e1335bd1
tanaypatil <tanaypatil197@gmail.com>
2023-09-14 23:44:09 +0530

    Time: 426 ms (9.37%), Space: 128.6 MB (5.35%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
@@ -7,7 +7,7 @@ class Solution:
             if i >= n:
                 return 0
             if not k:
-                return float('-inf') # if i < n else 0
+                return float('-inf')
             curr_avg = (curr_sum + nums[i])/(curr_count + 1)
             return max(curr_avg + dp(i+1, k-1, 0, 0), dp(i+1, k, (curr_count + 1), (curr_sum + nums[i])))
         
