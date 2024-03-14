7ab1c1bb79a19a4ae1cd3d5e41848a0956c73086
tanaypatil <tanaypatil197@gmail.com>
2024-03-14 21:40:03 +0530

    Time: 346 ms (50.49%), Space: 34 MB (34.88%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPossibleDivide(self, nums: List[int], k: int) -> bool:
         if len(nums) % k: return False
-        
         heapify(nums)
         counts = Counter(nums)
         for i in range((len(nums)//k)):
@@ -14,13 +13,9 @@ class Solution:
                             counts[first] -= 1
                             break
                     if first == -1: 
-                        # print("here1")
                         return False
-                    # print(i, j, first)
                 else:
-                    if counts[first+j]:
-                        counts[first+j] -= 1
-                    else: 
-                        # print("here2")
-                        return False
+                    if not counts[first+j]: return False
+                    counts[first+j] -= 1
+                    
         return True
