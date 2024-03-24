a250670495dd6b24b741a28c0aebe522d62d9c11
tanaypatil <tanaypatil197@gmail.com>
2024-03-24 18:23:11 +0530

    Time: 45 ms (96.67%), Space: 16.6 MB (70.34%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,23 +1,23 @@
 class Solution:
     def canPartitionKSubsets(self, nums, k):
-        n = len(nums)
+        s, n = sum(nums), len(nums)
         q, r = divmod(sum(nums), k)
         nums.sort(reverse=True)
         if r or nums[0] > q: return False
         
-        def dp(rest_k, mask, cur_sum = 0, next_index = 0):
+        
+        def dp(rest_k, mask, curr_sum=0, next_index=0):
             if rest_k == 1:
                 return True
             
-            if cur_sum == q:
+            if curr_sum == q:
                 return dp(rest_k-1, mask)
             
             for i in range(next_index, n):
-                if not (mask & (1 << i)) and nums[i] + cur_sum <= q:
-                    if dp(rest_k, mask | (1 << i), nums[i] + cur_sum, i+1):
+                if not (mask & (1<<i)) and curr_sum + nums[i] <= q:
+                    if dp(rest_k, mask | (1<<i), curr_sum + nums[i], i+1):
                         return True
-                    if cur_sum == 0:
-                        break
+                    if not curr_sum: break
             return False
         
         return dp(k, 0)
