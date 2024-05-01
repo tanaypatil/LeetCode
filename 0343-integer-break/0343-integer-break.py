917b64b6099abf22b932257fd378e122bf26fed2
tanaypatil <tanaypatil197@gmail.com>
2024-05-01 18:03:45 +0530

    Time: 768 ms (5.49%), Space: 255.6 MB (11.94%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -2,7 +2,7 @@ class Solution:
     def integerBreak(self, n: int) -> int:
         @lru_cache(None)
         def dp(i, p, s):
-            return (p if s == n else float('-inf')) if (i > (int(n**(0.5)) + 1) or s >= n) else max(float('-inf'), dp(i, p*i, s+i), dp(i+1, p*i, s+i), dp(i+1, p, s))
+            return (p if s == n else float('-inf')) if (i > (int(sqrt(n)) + 1) or s >= n) else max(float('-inf'), dp(i, p*i, s+i), dp(i+1, p*i, s+i), dp(i+1, p, s))
         return dp(1, 1, 0) if n > 2 else 1
             
         
