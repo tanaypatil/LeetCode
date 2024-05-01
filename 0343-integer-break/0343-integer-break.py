2d4b3b634c9222e8241a3f511c207c11ce063003
tanaypatil <tanaypatil197@gmail.com>
2024-05-01 18:02:53 +0530

    Time: 849 ms (5.49%), Space: 255.8 MB (11.94%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -3,6 +3,6 @@ class Solution:
         @lru_cache(None)
         def dp(i, p, s):
             return (p if s == n else float('-inf')) if (i > (int(n**(0.5)) + 1) or s >= n) else max(float('-inf'), dp(i, p*i, s+i), dp(i+1, p*i, s+i), dp(i+1, p, s))
-        return max(dp(1, 1, 0), 1) if n > 2 else 1
+        return dp(1, 1, 0) if n > 2 else 1
             
         
