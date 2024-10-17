ff19d69546876c17cc4465b67c5877ed56092a90
tanaypatil <tanaypatil197@gmail.com>
2024-10-17 20:19:25 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (26.33%) - LeetHub

--- 0593-valid-square/0593-valid-square.py
+++ 0593-valid-square/0593-valid-square.py
@@ -1,16 +1,7 @@
 class Solution:
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
-        
         def get_distance(p1, p2):
             return (p2[0]-p1[0])**2 + (p2[1]-p1[1])**2
-        
-        distances = [
-            get_distance(p1, p2),
-            get_distance(p1, p3),
-            get_distance(p1, p4),
-            get_distance(p2, p3),
-            get_distance(p2, p4),
-            get_distance(p3, p4)
-        ]
+        distances = [get_distance(p1, p2), get_distance(p1, p3), get_distance(p1, p4), get_distance(p2, p3), get_distance(p2, p4), get_distance(p3, p4)]
         distances.sort()
         return (distances[0] == distances[1] == distances[2] == distances[3]) and (distances[4] == distances[5]) and (2*distances[0] == distances[4]) and (distances[0] != 0)
