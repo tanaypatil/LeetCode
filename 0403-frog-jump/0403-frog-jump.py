9aeb5be9c85d93dec22e8f73a5f428a4917bd0f5
tanaypatil <tanaypatil197@gmail.com>
2024-11-04 00:46:22 +0530

    Time: 47 ms (89.06%), Space: 25.2 MB (37.17%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -3,16 +3,12 @@ class Solution:
         
         @lru_cache(None)
         def dp(i, last):
-            # print(i, last)
             if i >= len(stones)-1:
-                # print("Returning True")
                 return True
             
             for j in [last-1, last, last+1]:
-                if stones[i]+j in exists and j > 0:
-                    # print(f"From {stones[i]} to {stones[i]+j}")
-                    if dp(exists[stones[i]+j], j):
-                        return True
+                if stones[i]+j in exists and j > 0 and dp(exists[stones[i]+j], j):
+                    return True
             return False
         
         exists = {}
