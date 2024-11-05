7e98fda0ff063bde4b41a9bc0328e0274ecbec3f
tanaypatil <tanaypatil197@gmail.com>
2024-11-05 23:29:56 +0530

    Time: 2565 ms (23.82%), Space: 55.3 MB (79.23%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -5,14 +5,10 @@ class Solution:
         
         sieve = [1]*n
         sieve[:2] = [0, 0]
-        ans = 0
         sqt = int(sqrt(n))
         for i in range(2, sqt+1):
             if sieve[i]:
                 for j in range(i, (n//i)+1):
                     if i*j < n:
                         sieve[i*j] = 0
-            ans += 1 if sieve[i] else 0
-        for i in range(sqt+1, n):
-            ans += 1 if sieve[i] else 0
-        return ans
+        return sieve.count(1)
