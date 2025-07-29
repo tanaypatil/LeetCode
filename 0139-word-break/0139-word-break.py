6a6a10b4d05044959ba0d5b8a72a11ca00b254c3
tanaypatil <tanaypatil197@gmail.com>
2025-07-29 23:15:44 +0530

    Time: 0 ms (100%), Space: 17.9 MB (44.96%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,23 +1,16 @@
-from collections import Counter, defaultdict
-
-
 class Solution:
-    def find(self, i):
-        if i >= len(Solution.s):
-            return True
-        if i in Solution.mem:
-            return Solution.mem[i]
-        for word in Solution.wordDict:
-            if Solution.s[i] == word[0] and word == Solution.s[i:i+len(word)]:
-                if self.find(i+len(word)):
-                    return True
-        Solution.mem[i] = False
-        return False
-    
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        Solution.s = s
-        Solution.wordDict = wordDict
-        Solution.mem = {}
-        return self.find(0)
-        
-        
+
+        @lru_cache()
+        def dfs(i):
+            if i >= len(s):
+                return True
+
+            for word in wordDict:
+                if s[i:i+len(word)] == word:
+                    if dfs(i+len(word)):
+                        return True
+            
+            return False
+
+        return dfs(0)        
