730d58a344625889735b07778315fb90a52d4764
tanaypatil <tanaypatil197@gmail.com>
2025-07-29 23:17:13 +0530

    Time: 33 ms (0.22%), Space: 16.3 MB (100%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,11 +1,3 @@
 class Solution:
-    def countSetBits(self, n):
-        count = 0
-        while n > 0:
-            count+=n&1
-            n >>= 1
-            
-        return count
-    
     def minBitFlips(self, start: int, goal: int) -> int:
-        return self.countSetBits(start^goal)
+        return bin(start^goal)[2:].count("1")
