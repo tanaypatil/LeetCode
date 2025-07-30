c7ded3e07093e1bee0160854994bf814b12bf565
tanaypatil <tanaypatil197@gmail.com>
2025-07-30 21:12:00 +0530

    Time: 209 ms (7.17%), Space: 20.4 MB (13.93%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,23 +1,19 @@
-from collections import Counter
-
-
 class Solution:
-    def atMostK(self, nums, K):
-        res = 0
-        left = 0
-        counts = Counter()
-        for right, num in enumerate(nums):
-            if not counts[num]:
-                K -= 1
-            counts[num] += 1
-            while K < 0:
-                counts[nums[left]] -= 1
-                if not counts[nums[left]]:
-                    K += 1
-                left += 1
-            res += right-left+1
-        return res
-    
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        return self.atMostK(nums, k) - self.atMostK(nums, k-1)
+        def atMost(k):
+            ans = l = 0
+            counts = Counter()
+            for r in range(len(nums)):
+                if not counts[nums[r]]:
+                    k -= 1
+                counts[nums[r]] += 1
+                while k < 0:
+                    counts[nums[l]] -= 1
+                    if not counts[nums[l]]:
+                        k += 1
+                    l += 1
+                ans += (r-l+1)
+            return ans
+
+        return atMost(k) - atMost(k-1)
         
