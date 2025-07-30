ff1099562b4f6ec36225dd8ff3c7561e36bd7c36
tanaypatil <tanaypatil197@gmail.com>
2025-07-30 20:59:02 +0530

    Time: 173 ms (18.12%), Space: 17.8 MB (90.04%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,33 +1,12 @@
 class Solution:
-    def atMostK(self, nums, K):
-        res = 0
-        left = 0
-        counts = Counter()
-        for right, num in enumerate(nums):
-            if not counts[num]:
-                K -= 1
-            counts[num] += 1
-            while K < 0:
-                counts[nums[left]] -= 1
-                if not counts[nums[left]]:
-                    K += 1
-                left += 1
-            res += right-left+1
-        return res
-    
-    
     def numberOfSubstrings(self, s: str) -> int:
-        # left, right = 0, 0
-        # count = 0
-        # freq = {}
-        # for right, c in enumerate(s):
-        #     freq[c] = freq.get(c, 0) + 1
-        #     while len(freq) >= 3:
-        #         freq[s[left]] -= 1
-        #         if freq[s[left]] == 0:
-        #             del freq[s[left]]
-        #         left += 1
-        #     count += left
-        # return count
+        counts = Counter()
+        ans = l = 0
+        for r in range(len(s)):
+            counts[s[r]] += 1
+            while counts["a"] and counts["b"] and counts["c"]:
+                ans += len(s) - r
+                counts[s[l]] -= 1
+                l += 1
+        return ans
         
-        return self.atMostK(s, 3) - self.atMostK(s, 2)
