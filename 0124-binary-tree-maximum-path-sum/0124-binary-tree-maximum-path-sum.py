9d37bf004792613f839ec68d93dc3cefc34e0999
tanaypatil <tanaypatil197@gmail.com>
2025-07-30 23:14:34 +0530

    Time: 25 ms (7.22%), Space: 23.2 MB (13.86%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -4,24 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def mps(root, m):
-    if not root:
-        return 'r'
-    left = mps(root.left, m)
-    right = mps(root.right, m)
-    
-    m[0] = max(m[0], -9999999999 if left == 'r' else left, -99999999 if right=='r' else right, root.val)
-    left = 0 if left=='r' else left
-    right = 0 if right=='r' else right
-    
-    m[0] = max(m[0], max(left, right)+root.val, left+right+root.val)
-    
-    return max(max(left, right) + root.val, root.val)
-
-
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        m = [-999999999999]
-        mps(root, m)
-        return m[0]
+        def dfs(root):
+            if not root:
+                return float('-inf')
+            left, right = dfs(root.left), dfs(root.right)
+            ans[0] = max(ans[0], root.val + left + right, root.val + max(left, right, 0))
+            return root.val + max(left, right, 0)
+        ans = [float('-inf')]
+        dfs(root)
+        return ans[0] if ans[0] != float('-inf') else 0
+
         
