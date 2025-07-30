746e1f5594092d9ad28d349861b9f10ede2d57a5
tanaypatil <tanaypatil197@gmail.com>
2025-07-30 20:49:50 +0530

    Time: 30 ms (64.88%), Space: 21.4 MB (48.52%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,14 +1,11 @@
-from collections import defaultdict
-
-
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        s = 0
-        count = 0
-        freq = defaultdict(int)
-        freq[0] = 1
+        smap = {0:1}
+        ans = s = 0
         for num in nums:
             s += num
-            count += freq[s-goal]
-            freq[s] += 1
-        return count
+            if s-goal in smap:
+                ans += smap[s-goal]
+            smap[s] = smap.get(s, 0) + 1
+        return ans
+        
