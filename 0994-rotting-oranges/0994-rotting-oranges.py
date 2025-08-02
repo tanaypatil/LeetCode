0f3065b88dd9a37075b92d53ccadf339d51b576e
tanaypatil <tanaypatil197@gmail.com>
2025-08-02 13:44:51 +0530

    Time: 0 ms (100%), Space: 17.8 MB (71.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,34 @@
-from collections import deque, defaultdict
-
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue = deque()
-        rows, cols = len(grid), len(grid[0])
-        fresh = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 2:
-                    queue.append((i, j))
-                elif grid[i][j] == 1:
+        fresh, q = 0, deque()
+        m, n = len(grid), len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
                     fresh += 1
-        
-        minutes = -1
-        mem = defaultdict(int)
-        while queue:
-            l = len(queue)
-            for i in range(l):
-                row, col = queue.popleft()
-                r = [-1, 1, 0, 0]
-                c = [0, 0, -1, 1]
-                for p in range(4):
-                    a, b = row + r[p], col + c[p]
-                    # print(a, b, 0 <= a < rows, 0 <= b <= cols)
-                    if 0 <= a < rows and 0 <= b < cols and grid[a][b] == 1 and not mem[(a, b)]:
-                        queue.append((a, b))
-                        fresh -= 1
-                        mem[(a, b)] = 1
-            minutes += 1
-            
-        return (minutes if minutes > 0 else 0) if not fresh else -1
-                
+                elif grid[i][j] == 2:
+                    q.append((i, j))
+
+        def move(i, j):
+            moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            for di, dj in moves:
+                r, c = i + di, j + dj
+                if 0 <= r < m and 0 <= c < n and grid[r][c] == 1:
+                    yield r, c
+
+        time = 0
+        while q:
+            s = len(q)
+            for _ in range(s):
+                i, j = q.popleft()
+                grid[i][j] = 0
+                for x, y in move(i, j):
+                    fresh -= 1
+                    grid[x][y] = 0
+                    q.append((x, y))
+
+            if q:
+                time += 1
+
+        return time if not fresh else -1
