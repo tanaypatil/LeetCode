71419c9d9731354efb214cf230128ff52d531327
tanaypatil <tanaypatil197@gmail.com>
2025-08-25 23:18:14 +0530

    Time: 4 ms (13.29%), Space: 18 MB (69.37%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,20 @@
-def genP(n, stack, s, ans, openingPCount):
-    if len(s) >= n:
-        if not stack:
-            ans.append(s)
-        return
-    if openingPCount < n//2:
-        genP(n, stack+["("], s+"(", ans, openingPCount+1)
-    p = stack.pop() if stack else None
-    if p and p == "(":
-        genP(n, stack, s+")", ans, openingPCount)
-        
-        
-
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+
+        def dfs(s, stack):
+            if len(s) == 2*n:
+                if not stack:
+                    ans.append(s)
+                return
+            
+            if s.count("(") < n:
+                dfs(s + "(", stack + ["("])
+            if stack:
+                stack.pop()
+                dfs(s + ")", stack)
+
         ans = []
-        genP(2*n, [], "", ans, 0)
+        dfs("", [])
         return ans
+
+        
