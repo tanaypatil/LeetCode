186129d1ed77ea4bd7546b44a5b70d3f639cdf00
tanaypatil <tanaypatil197@gmail.com>
2025-08-25 23:24:32 +0530

    Time: 0 ms (100%), Space: 17.7 MB (86.08%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,10 +1,3 @@
-def reverse(nums):
-    i = 0
-    n = len(nums)
-    for i in range(((n-1)//2)+1):
-        nums[i], nums[n-i-1] = nums[n-i-1], nums[i]
-
-
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
@@ -12,20 +5,23 @@ class Solution:
         """
         n = len(nums)
         i = n-1
-        p = -1
-        k = -1
-        while i > 0:
-            j = i-1
-            while j != -1 and nums[j] >= nums[i]:
-                j -= 1
-            if j != -1:
-                if j > p:
-                    p = j
-                    k = i
+
+        while i > 0 and nums[i-1] >= nums[i]:
             i -= 1
-        if p == -1:
-            reverse(nums)
-        else:
-            nums[k], nums[p] = nums[p], nums[k]
-            nums[p+1:] = list(reversed(nums[p+1:]))
+
+        if i == 0:
+            nums.reverse()
+            return
+
+        j = i
+
+        while j+1 < n and nums[j+1] > nums[i-1]:
+            j += 1
+
+        nums[j], nums[i-1] = nums[i-1], nums[j]
+        nums[i:] = nums[i:][::-1]
+
+        
+        
+        
         
