364d634f2b187f97cd37b054a264077df968ba0a
tanaypatil <tanaypatil197@gmail.com>
2025-08-25 23:29:46 +0530

    Time: 42 ms (7.74%), Space: 18 MB (39.88%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,24 +1,18 @@
-def genSum(arr, target_sum, current_sum, index, current_res, res, memo):
-    if current_sum > target_sum or index >= len(arr):
-        return
-    
-    if current_sum + arr[index] == target_sum:
-        z = tuple(sorted(current_res+[arr[index]]))
-        if z not in memo:
-            res.append(z)
-            memo[z] = 1
-        
-    if current_sum + arr[index] < target_sum:
-        genSum(arr, target_sum, current_sum+arr[index], index, current_res+[arr[index]], res, memo)
-        genSum(arr, target_sum, current_sum+arr[index], index+1, current_res+[arr[index]], res, memo)
-    genSum(arr, target_sum, current_sum, index+1, current_res, res, memo)
-
-
 class Solution:
-    def combinationSum(self, A: List[int], B: int) -> List[List[int]]:
-        res = []
-        memo = {}
-        A = list(set(A))
-        A.sort()
-        genSum(A, B, 0, 0, [], res, memo)
-        return res
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        def dfs(i, s, path):
+            if i >= len(candidates):
+                if s == target:
+                    res.add(tuple(path))
+                return
+            
+            if s + candidates[i] <= target:
+                dfs(i+1, s+candidates[i], path+[candidates[i]])
+                dfs(i, s+candidates[i], path+[candidates[i]])
+            
+            dfs(i+1, s, path)
+            
+        res = set()
+        dfs(0, 0, [])
+        return list(map(list, res))
