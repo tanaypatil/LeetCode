7155c0ed8461fc472a4cfe52e5a6f99049b69cdc
tanaypatil <tanaypatil197@gmail.com>
2025-09-18 20:32:15 +0530

    Time: 0 ms (100%), Space: 18.1 MB (21.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,12 @@
-def genStr(s, t, res, i):
-    if i >= len(s):
-        return
-    res.append(t+[s[i]])
-    genStr(s, t+[s[i]], res, i+1)
-    genStr(s, t, res, i+1)
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        genStr(nums, [], res, 0)
+        res = []
+        for i in range(2**(len(nums))):
+            bs = bin(i)[2:]
+            bs = "0"*(len(nums)-len(bs)) + bs
+            curr = []
+            for p, j in enumerate(bs):
+                if j != "0":
+                    curr.append(nums[p])
+            res.append(curr)
         return res
