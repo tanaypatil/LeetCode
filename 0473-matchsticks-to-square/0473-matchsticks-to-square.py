17dd2b2d427bd867cb044b15ec341d4557c2817a
tanaypatil <tanaypatil197@gmail.com>
2025-09-18 23:37:22 +0530

    Time: 14 ms (99.58%), Space: 18.2 MB (16.97%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -1,24 +1,25 @@
 class Solution:
     def makesquare(self, nums: List[int]) -> bool:
-        n = len(nums)
-        q, r = divmod(sum(nums), 4)
+        s, n = sum(nums), len(nums)
+        q, r = divmod(s, 4)
         nums.sort(reverse=True)
         if r or nums[0] > q: return False
         
-        def dp(left_k, mask, curr_sum=0, next_index=0):
-            if left_k == 1:
+        @lru_cache(None)
+        def dp(k, mask=0, cs=0, j=0):
+            if mask == (1<<n)-1:
                 return True
             
-            if curr_sum == q:
-                return dp(left_k-1, mask)
+            if cs == q:
+                return dp(k-1, mask)
             
-            for i in range(next_index, n):
-                if not (mask & (1<<i)) and nums[i] + curr_sum <= q:
-                    if dp(left_k, mask | 1<<i, curr_sum + nums[i], i+1):
+            for i in range(j, n):
+                if not (mask & 1<<i) and cs+nums[i] <= q:
+                    if dp(k, mask | 1<<i, cs+nums[i], i+1):
                         return True
-                    if not curr_sum: break
-                    
+                    if not cs:
+                        break
+                        
             return False
         
         return dp(4, 0)
-                    
