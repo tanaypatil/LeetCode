a87df11297dc9957d608523fbb57ea622e07cc38
tanaypatil <tanaypatil197@gmail.com>
2025-09-18 21:20:38 +0530

    Time: 7 ms (78.64%), Space: 18.5 MB (75.62%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,20 +1,15 @@
-from collections import deque, defaultdict
-
-
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
         max_reach = nums[0]
+        jumps = 1
         end = nums[0]
         
-        jumps = 1
+        if len(nums) <= 1: return 0
         
-        if n <= 1:
-            return 0
-        
-        for i in range(1, n-1):
-            max_reach = max(i + nums[i], max_reach)
+        for i in range(1, len(nums)-1):
+            max_reach = max(max_reach, i+nums[i])
             if i == end:
                 end = max_reach
                 jumps += 1
         return jumps
+        
