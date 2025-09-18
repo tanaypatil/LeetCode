e38670edde658ab8efb18d4ddc495e74681349b4
tanaypatil <tanaypatil197@gmail.com>
2025-09-18 23:01:40 +0530

    Time: 55 ms (51.35%), Space: 22.4 MB (7.23%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -4,28 +4,23 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-def lca(root, p, q, res):
-    if not root:
-        return False
-    
-    left, right = lca(root.left, p, q, res), lca(root.right, p, q, res)
-    
-    if left and right and not res[0]:
-        res[0] = root
-        return True
-    
-    if (left or right) and (root.val == p.val or root.val == q.val) and not res[0]:
-        res[0] = root
-        return True
-    
-    if root.val == p.val or root.val == q.val:
-        return True
-    
-    return left or right
-
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        res = [None]
-        lca(root, p, q, res)
-        return res[0]
+        self.lca = None
+        def dfs(node):
+            if not node:
+                return False, False
+            
+            leftp, leftq = dfs(node.left)
+            rightp, rightq = dfs(node.right)
+            
+            foundp, foundq = leftp or rightp or node.val == p.val, leftq or rightq or node.val == q.val
+            
+            if not self.lca and foundp and foundq:
+                self.lca = node
+                
+            return foundp, foundq
+        dfs(root)
+        return self.lca
+            
