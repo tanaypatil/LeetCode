c7bfa8da607f4936a3d72f9bde983d43b07834a0
tanaypatil <tanaypatil197@gmail.com>
2025-09-18 23:29:52 +0530

    Time: 19 ms (95.97%), Space: 25 MB (76.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,22 +1,20 @@
 class Solution:
-    def dfs(self, index, path):
-        if (index >= len(Solution.s)) or (len(path) >= len(Solution.t)):
-            return 1 if path == Solution.t else 0
-        if len(Solution.s)-index + len(path) < len(Solution.t):
-            return 0
-        if (index, path) in Solution.mem:
-            return Solution.mem[(index, path)]
-        taken = 0
-        if Solution.s[index] == Solution.t[len(path)]:
-            taken += self.dfs(index+1, path+Solution.s[index])
-        not_taken =  self.dfs(index+1,  path)
-        c = taken + not_taken
-        Solution.mem[(index, path)] = c
-        return c
-           
-        
     def numDistinct(self, s: str, t: str) -> int:
-        Solution.mem = {}
-        Solution.s, Solution.t = s, t
-        return self.dfs(0, "")
+        
+        @lru_cache(None)
+        def dp(i, curr):
+            # print(i, curr)
+            if i >= len(s):
+                return 1 if curr == t else 0
+            
+            ans = 0
+            if len(s)-i >= len(t)-len(curr):
+                ans += dp(i+1, curr)
+            
+            if len(curr) < len(t) and s[i] == t[len(curr)]:
+                ans += dp(i+1, curr+s[i])
+                
+            return ans
+        
+        return dp(0, "")
         
