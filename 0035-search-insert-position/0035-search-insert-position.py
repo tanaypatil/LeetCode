e856fe6c001e1e86bf088576ee6baad1e3a54274
tanaypatil <tanaypatil197@gmail.com>
2026-02-14 17:45:38 +0530

    Time: 0 ms (100%), Space: 20 MB (56.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,4 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect_left(nums, target)
+        def b_left(nums, target):
+            l, r = 0, len(nums)
+            while l < r:
+                mid = (l + r) // 2
+                if target <= nums[mid]:
+                    r = mid
+                else:
+                    l = mid + 1
+            return l
+        return b_left(nums, target)
         
