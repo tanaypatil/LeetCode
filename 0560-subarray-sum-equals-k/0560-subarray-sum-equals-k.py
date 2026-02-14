02580dbdc4b9cb22c3bd8e15fc918e51bfebc646
tanaypatil <tanaypatil197@gmail.com>
2026-02-14 17:32:57 +0530

    Time: 30 ms (74.24%), Space: 22 MB (26.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,11 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        hm = {0: 1}
-        s = c = 0
+        smap = {0:1}
+        s, ans = 0, 0
         for num in nums:
             s += num
-            if s-k in hm:
-                c += hm[s-k]
-            hm[s] = hm.get(s, 0) + 1
-        return c
+            if s-k in smap:
+                ans += smap[s-k]
+            smap[s] = smap.get(s, 0) + 1
+        return ans
         
