89e040331d89a9ab5599066b7d86615b3399a58f
tanaypatil <tanaypatil197@gmail.com>
2026-02-15 00:57:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (24.29%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,16 +1,16 @@
-def getSumSet(num, current_sum, path, target_sum, target_len, res):
-    if num > 9:
-        return
-    if current_sum + num == target_sum and len(path) + 1 == target_len:
-        res.add(tuple(path+[num]))
-    if current_sum + num < target_sum and len(path) + 1 < target_len:
-        getSumSet(num+1, current_sum+num, path+[num], target_sum, target_len, res)
-    if current_sum < target_sum and len(path) < target_len:
-        getSumSet(num+1, current_sum, path, target_sum, target_len, res)
-
-
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res = set()
-        getSumSet(1, 0, [], n, k, res)
-        return res
+
+        def dfs(i, s, path):
+            if i > 9:
+                if s == n and len(path) == k:
+                    ans.add(path)
+                return
+
+            if i + s <= n and len(path) + 1 <= k:
+                dfs(i+1, s+i, tuple(list(path)+[i]))
+            dfs(i+1, s, path)
+        
+        ans = set()
+        dfs(1, 0, tuple())
+        return list(ans)
