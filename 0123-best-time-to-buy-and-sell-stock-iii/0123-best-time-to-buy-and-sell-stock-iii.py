e4fe4243749fc289f9ef21b9af18a8d6c0d06af1
tanaypatil <tanaypatil197@gmail.com>
2026-02-16 20:46:18 +0530

    Time: 304 ms (38.59%), Space: 31.1 MB (55.1%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,15 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def maxProfitK(k, prices):
-            n = len(prices)
-            prev = [0]*n
+        # T(i, j) = max(T(i, j-1), prices[j]-prices[m]+T(i-1, m))
+        
+        n = len(prices)
+        
+        def kTransactionMax(k):
+            prev = [0]*n    
             for i in range(k):
+                max_diff = -prices[0]
                 dp = [0]*n
-                maxDiff = -prices[0]
                 for j in range(1, n):
-                    dp[j] = max(dp[j-1], prices[j]+maxDiff)
-                    maxDiff = max(maxDiff, prev[j]-prices[j])
+                    dp[j] = max(dp[j-1], prices[j] + max_diff)
+                    max_diff = max(max_diff, prev[j]-prices[j])
                 prev = dp
             return prev[-1]
-        return maxProfitK(2, prices)
+        
+        return kTransactionMax(2)
+                    
         
